41e9f77a1491c1bebed9ac0871c17ff4c1c9d7ab
Mqksm <itismyfriend@gmail.com>
2020-03-13 20:00:09 +0200

    Initial Commit

---
+++ Localization/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Localization
+//
+//  Created by Maks on 13.03.2020.
+//  Copyright © 2020 Maxim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
