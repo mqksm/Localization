08e3078be5352def987355a9b4ab5406f22331d1
Mqksm <itismyfriend@gmail.com>
2020-03-19 21:01:56 +0200

    First commit

--- Localization/ViewController.swift
+++ Localization/ViewController.swift
@@ -7,14 +7,57 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager: CLLocationManager = CLLocationManager()
+    let geocoder = CLGeocoder()
+    var placemark: CLPlacemark?
+    
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var AltitudeLabel: UILabel!
+    @IBOutlet weak var currentAddressLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation // set accuracy of localiztion
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.distanceFilter  = 1 // Set location update frequency. In this case, 1 meter
+        locationManager.pausesLocationUpdatesAutomatically = true // enable to detect the situation where an app doesn’t need location update for battery saving
+        locationManager.activityType = .fitness // The type of user activity associated with the location updates
+        locationManager.startUpdatingLocation()
+        
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let currentLocation = locations.last else { return }
+        //            let coords = [ "latitude": currentLocation.coordinate.latitude, "longitude": currentLocation.coordinate.longitude, "altitude": currentLocation.altitude, "horizontalAccuracy": currentLocation.horizontalAccuracy, "verticalAccuracy": currentLocation.verticalAccuracy]
+        latitudeLabel.text = String(format:"%.8f", currentLocation.coordinate.latitude)
+        longitudeLabel.text = String(format:"%.8f", currentLocation.coordinate.longitude)
+        AltitudeLabel.text = String(format:"%.8f", currentLocation.altitude)
+        
+        geocoder.reverseGeocodeLocation(currentLocation) { (placemarks, error) in
+            guard let placemark = placemarks?.last else { return }
+            let country = String(placemark.country ?? "")
+            let city = String(placemark.locality ?? "")
+            let city2 = String(placemark.subLocality ?? "")
+            let street = String(placemark.thoroughfare ?? "")
+            let house = String(placemark.subThoroughfare ?? "")
+            let adrress = country + ", " + city + ", " + city2 + ", " + street + ", " + house
+            self.currentAddressLabel.text = adrress
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print (error)
+        print ("Can't get your location")
+    }
+    
 }
+
 
